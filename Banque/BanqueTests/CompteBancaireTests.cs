3e6f426df94226c74fb2c5c26b700ba231b298dc
SamathSeck <108096354+SamathSeck@users.noreply.github.com>
2024-11-09 14:54:49 +0000

    commit

---
+++ Banque/BanqueTests/CompteBancaireTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompteBanqueNS;
+
+namespace BanqueTests
+{
+    [TestClass]
+    public class CompteBancaireTests
+    {
+        [TestMethod]
+        public void VérifierDébitCompteCorrect()
+        {
+            // Ouvrir un compte
+            double soldeInitial = 500000;
+            double montantDébit = 400000;
+            double soldeAttendu = 100000;
+
+            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+
+            // Débiter
+            compte.Débiter(montantDébit);
+
+            // Tester
+            double soldeObtenu = compte.Balance;
+            Assert.AreEqual(soldeAttendu, soldeObtenu, 0.001, "Compte débité incorrectement");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DébiterMontantNégatifLèveArgumentOutOfRange()
+        {
+            double soldeInitial = 500000;
+            double montantDébit = -10;
+            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+
+            compte.Débiter(montantDébit);
+        }
+    }
+
+}
---
+++ Banque/BanqueTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BanqueTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BanqueTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a3bd1c2-82d6-4bdf-8258-142060a23bd7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
